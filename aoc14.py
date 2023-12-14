c80a961f55a7369aa79720fc26d20599cea7f905
Remo Williams <remo@qlac.ch>
2023-12-14 20:35:19 +0100

    Day 14, part 2.

--- aoc14.py
+++ aoc14.py
@@ -15,51 +15,143 @@ def get_input(filename):
 
 
 def parse_platform(data):
-    platform = []
-    for line in data:
-        platform.append(line)
-    return platform
-
-
-def slide_stone(row, col, platform):
-    for slide in range(row, 0, -1):
-        if platform[slide - 1][col] == ".":
-            platform[slide] = platform[slide][:col] + "." + platform[slide][col + 1 :]
-            platform[slide - 1] = (
-                platform[slide - 1][:col] + "O" + platform[slide - 1][col + 1 :]
-            )
-        else:
-            break
+    height = len(data)
+    width = len(data[0])
+    stones = {}
+    for row, line in enumerate(data):
+        for col, char in enumerate(line):
+            if char != ".":
+                stones[(row, col)] = char
+    return height, width, stones
 
 
 def tilt_north(platform):
-    for row, line in enumerate(platform):
-        for col, char in enumerate(line):
-            if char == "O":
-                slide_stone(row, col, platform)
+    height, width, stones = platform
+    tilted_stones = {}
+    for row in range(height):
+        for col in range(width):
+            if (row, col) in stones:
+                if stones[(row, col)] == "O":
+                    for slided_row in range(row - 1, -1, -1):
+                        if (slided_row, col) in tilted_stones:
+                            tilted_stones[(slided_row + 1, col)] = "O"
+                            break
+                    else:
+                        tilted_stones[(0, col)] = "O"
+                else:
+                    tilted_stones[(row, col)] = "#"
+    return height, width, tilted_stones
+
+
+def tilt_west(platform):
+    height, width, stones = platform
+    tilted_stones = {}
+    for col in range(width):
+        for row in range(height):
+            if (row, col) in stones:
+                if stones[(row, col)] == "O":
+                    for slided_col in range(col - 1, -1, -1):
+                        if (row, slided_col) in tilted_stones:
+                            tilted_stones[(row, slided_col + 1)] = "O"
+                            break
+                    else:
+                        tilted_stones[(row, 0)] = "O"
+                else:
+                    tilted_stones[(row, col)] = "#"
+    return height, width, tilted_stones
+
+
+def tilt_south(platform):
+    height, width, stones = platform
+    tilted_stones = {}
+    for row in range(height - 1, -1, -1):
+        for col in range(width):
+            if (row, col) in stones:
+                if stones[(row, col)] == "O":
+                    for slided_row in range(row + 1, height):
+                        if (slided_row, col) in tilted_stones:
+                            tilted_stones[(slided_row - 1, col)] = "O"
+                            break
+                    else:
+                        tilted_stones[(height - 1, col)] = "O"
+                else:
+                    tilted_stones[(row, col)] = "#"
+    return height, width, tilted_stones
+
+
+def tilt_east(platform):
+    height, width, stones = platform
+    tilted_stones = {}
+    for col in range(width - 1, -1, -1):
+        for row in range(height):
+            if (row, col) in stones:
+                if stones[(row, col)] == "O":
+                    for slided_col in range(col + 1, width):
+                        if (row, slided_col) in tilted_stones:
+                            tilted_stones[(row, slided_col - 1)] = "O"
+                            break
+                    else:
+                        tilted_stones[(row, width - 1)] = "O"
+                else:
+                    tilted_stones[(row, col)] = "#"
+    return height, width, tilted_stones
 
 
 def calc_weight(platform):
-    load = len(platform)
+    height, _, stones = platform
     weight = 0
-    for line in platform:
-        for char in line:
-            if char == "O":
-                weight += load
-        load -= 1
+    for pos in stones:
+        if stones[pos] == "O":
+            weight += height - pos[0]
     return weight
+
+
+def print_platform(platform):
+    height, width, stones = platform
+    for row in range(height):
+        for col in range(width):
+            if (row, col) in stones:
+                print(stones[(row, col)], end="")
+            else:
+                print(".", end="")
+        print()
 
 
 def part1(data):
+    # print()
     platform = parse_platform(data)
-    tilt_north(platform)
-    # print(platform)
-    weight = calc_weight(platform)
+    # print_platform(platform)
+    # print()
+    tp = tilt_north(platform)
+    # print_platform(tp)
+    weight = calc_weight(tp)
     return weight
 
 
+def cycle_platform(platform):
+    return tilt_east(tilt_south(tilt_west(tilt_north(platform))))
+
+
 def part2(data):
-    return None
+    # print()
+    platform = parse_platform(data)
+    # print()
+    # print_platform(platform)
+    weights = []
+    stones = []
+    for cycle in range(1000000000):
+        platform = cycle_platform(platform)
+        for idx in range(len(stones) - 1, -1, -1):
+            if stones[idx] == platform[2]:
+                # print(f"Cycle from {idx} to {cycle}.")
+                # print(weights)
+                return weights[idx + (1000000000 - idx - 1) % (cycle - idx)]
+        stones.append(platform[2])
+        # print_platform(platform)
+        # print()
+        weights.append(calc_weight(platform))
+    # print(weights)
+    return weights[-1]
 
 
 def run_tests():
@@ -98,7 +190,7 @@ def run_part2(solved):
 def main():
     run_tests()
     run_part1(True)
-    # run_part2(False)
+    run_part2(True)
 
 
 if __name__ == "__main__":
